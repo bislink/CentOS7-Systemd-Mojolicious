1d166970b1b2658205b18b9b8548be3c180194d3
root@gap.a1z.us <root@gap.a1z.us>
2020-07-12 09:52:24 +0000

    020 repo: github

--- r
+++ r
@@ -1,2 +1,7 @@
 git push -u hop master
 git push -u hop master --tags
+
+git push -u github master
+git push -u github master --tags
+
+
--- start_apps.sh
+++ start_apps.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/sh
-
+echo "Starting apps/services"
+echo ""
 hypnotoad /app/perlmojo/script/perlmojo
-
+echo ""
 systemctl start mariadb
-
+echo ""
 systemctl start httpd
-
+echo ""
 wget --spider --tries 1 --no-check-certificate https://gap.a1z.us:7312
-
+echo "";
 wget --spider --tries 1 --no-check-certificate https://gap.a1z.us:7318
+echo ""
